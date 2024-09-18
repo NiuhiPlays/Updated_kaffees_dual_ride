9bd5628d82abe9cf0768a5161c0e03d50bcc7fb4
Kaffee <tobi.von.massow@gmail.com>
2024-09-18 11:11:58 +0200

    support 1.20.5 to 1.21.1

--- src/main/java/de/tobiasvonmassow/kaffees_dual_ride/mixin/Kaffees_Dual_Ride_Mixin.java
+++ src/main/java/de/tobiasvonmassow/kaffees_dual_ride/mixin/Kaffees_Dual_Ride_Mixin.java
@@ -8,15 +8,11 @@ import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractHorseEntity.class)
 public abstract class Kaffees_Dual_Ride_Mixin extends AnimalEntity {
@@ -25,24 +21,19 @@ public abstract class Kaffees_Dual_Ride_Mixin extends AnimalEntity {
 	private float lastAngryAnimationProgress;
 
 	protected Kaffees_Dual_Ride_Mixin(EntityType<? extends AbstractHorseEntity> arg, World arg2) {
-		super((EntityType<? extends AnimalEntity>) arg, arg2);
+		super(arg, arg2);
 	}
 
-	@Invoker("getPassengerAttachmentY")
-	protected abstract float invokeGetPassengerAttachmentY(EntityDimensions dimensions, float scaleFactor);
-
-	@Inject(method = "getPassengerAttachmentPos", at = @At(value = "HEAD", target = "Lnet/minecraft/entity/passive/AbstractHorseEntity;getPassengerAttachmentPos(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/EntityDimensions;F)Lorg/joml/Vector3f;"), cancellable = true)
-	private void injected(Entity passenger, EntityDimensions dimensions, float scaleFactor, CallbackInfoReturnable<Vector3f> cir) {
-		int i = ((AbstractHorseEntity) (Object) this).getPassengerList().indexOf(passenger);
+	@Override
+	public Vec3d getPassengerAttachmentPos(Entity passenger, EntityDimensions dimensions, float scaleFactor) {
+		int i = this.getPassengerList().indexOf(passenger);
 		float offset = 0;
-		if (((AbstractHorseEntity) (Object) this).getPassengerList().size() > 1) {
+		if (this.getPassengerList().size() > 1) {
 			offset = i == 0 ? 0.2f : -0.6f;
 		}
-		cir.setReturnValue(new Vector3f(0.0f, invokeGetPassengerAttachmentY(dimensions, scaleFactor) + 0.15f * lastAngryAnimationProgress * scaleFactor, -0.7f * lastAngryAnimationProgress * scaleFactor + offset));
+		return super.getPassengerAttachmentPos(passenger, dimensions, scaleFactor).add((new Vec3d(0.0, 0.15f * lastAngryAnimationProgress * scaleFactor, -0.7f * lastAngryAnimationProgress * scaleFactor + offset)).rotateY(-this.getYaw() * 0.017453292F));
 	}
 
-	// abstract class + constructor required, so I can extend AnimalEntity to
-	// perform this explicit override
 	@Override
 	public ActionResult interactMob(PlayerEntity player, Hand hand) {
 		if (this.canAddPassenger(player)) {
@@ -54,11 +45,7 @@ public abstract class Kaffees_Dual_Ride_Mixin extends AnimalEntity {
 
 	@Override
 	protected boolean canAddPassenger(Entity passenger) {
-		return this.getPassengerList().size() < this.getMaxPassengers();
-	}
-
-	protected int getMaxPassengers() {
-		return 2;
+		return this.getPassengerList().size() < 2;
 	}
 
 }
